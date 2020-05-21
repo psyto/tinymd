2b40ef8350e3ea09a8bdaf6cba732136b320f2d3
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2020-05-22 01:51:10 +0900

    chapter 3 completed

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
-fn get_version() -> u16 {
-    1000
+fn parse_markdown_file() {}
+
+fn print_short_banner() {
+    println!("{}", get_title());
 }
 
-fn usage() {
-    let the_version = get_version();
-    println!("tinymd, a markdown compiler written by Hiroyuki Saito");
-    println!("Version {}", the_version);
+// print_short_banner()
+// "Written by: " + env!("CARGO_PKG_AUTHORS")
+// "Homepage: " + env!("CARGO_PKG_HOMEPAGE")
+// "Usage: tinymd <somefile>.md"
+fn print_long_banner() {
+    print_short_banner();
+    println!(
+        "Written by: {}¥nHomepage: {}¥nUsage: tinymd <somefile>.md",
+        env!("CARGO_PKG_AUTHORS"),
+        env!("CARGO_PKG_HOMEPAGE")
+    );
 }
+
+// [TITLE] (v[VERSION]), [DESCRIPTION]
+fn get_title() -> String {
+    let mut the_title = String::from(env!("CARGO_PKG_NAME"));
+    the_title.push_str(" (v");
+    the_title.push_str(env!("CARGO_PKG_VERSION"));
+    the_title.push_str("), ");
+    the_title.push_str(env!("CARGO_PKG_DESCRIPTION"));
+    the_title
+}
+
+fn usage() {}
 
 fn main() {
     usage();
